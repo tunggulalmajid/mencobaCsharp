8ebc5a356d180b5fa029921c21f2ecdf2e295907
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-27 18:00:27 +0700

    coba aja

--- cobanet/Program.cs
+++ cobanet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +10,33 @@ namespace cobanet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("masukkan kata >> ");
+            Console.Write("masukkan kata >> ");
             string tanya = Console.ReadLine();
             Console.WriteLine($"kata yang anda masukkan adalah {tanya}");
+            if (tanya.Length > 5) 
+            {
+                Console.WriteLine("kata yang anda masukkan lebih dari 5 karakter");
+            }
+            else
+            {
+                Console.WriteLine("kata yang anda masukkan kurang dari 5 karakter");
+            }
+            Console.Write("Masukkan angka >> ");
+            int angka = Convert.ToInt32(Console.ReadLine());
+            if (angka < tanya.Length)
+            {
+                Console.WriteLine($"{angka} lebih kecil daripada {tanya.Length}");
+            }
+            else if (angka == tanya.Length)
+            {
+                Console.WriteLine($"{angka} sama dengan {tanya.Length}");
+            }
+            else
+            {
+                Console.WriteLine($"{angka} lebih besar daripada {tanya.Length}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
