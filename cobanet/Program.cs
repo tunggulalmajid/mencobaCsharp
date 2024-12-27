9d91d79bef0ef45cef17534428d74b1c8f366d6d
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-27 16:09:51 +0700

    Add project files.

---
+++ cobanet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cobanet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("masukkan kata >> ");
+            string tanya = Console.ReadLine();
+            Console.WriteLine($"kata yang anda masukkan adalah {tanya}");
+        }
+    }
+}
