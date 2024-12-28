79d4a3d2c63abfa92708e48a8826f93a952beaba
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-28 21:07:39 +0700

    login

--- cobanet/Program.cs
+++ cobanet/Program.cs
@@ -10,92 +10,114 @@ namespace cobanet
     {
         static void Main(string[] args)
         {
-            Console.Write("masukkan kata >> ");
-            // inputan
-            string tanya = Console.ReadLine();
-            Console.WriteLine($"kata yang anda masukkan adalah {tanya}");
-            
-            // perkondisian
-            if (tanya.Length > 5) 
-            {
-                Console.WriteLine("kata yang anda masukkan lebih dari 5 karakter");
-            }
-            else
-            {
-                Console.WriteLine("kata yang anda masukkan kurang dari 5 karakter");
-            }
-            Console.Write("Masukkan angka >> ");
+            //Console.Write("masukkan kata >> ");
+            //// inputan
+            //string tanya = Console.ReadLine();
+            //Console.WriteLine($"kata yang anda masukkan adalah {tanya}");
 
-            //casting ke int
-            int angka = Convert.ToInt32(Console.ReadLine());
-            if (angka < tanya.Length)
-            {
-                Console.WriteLine($"{angka} lebih kecil daripada {tanya.Length}");
-            }
-            else if (angka == tanya.Length)
-            {
-                Console.WriteLine($"{angka} sama dengan {tanya.Length}");
-            }
-            else
-            {
-                Console.WriteLine($"{angka} lebih besar daripada {tanya.Length}");
-            }
+            //// perkondisian
+            //if (tanya.Length > 5) 
+            //{
+            //    Console.WriteLine("kata yang anda masukkan lebih dari 5 karakter");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("kata yang anda masukkan kurang dari 5 karakter");
+            //}
+            //Console.Write("Masukkan angka >> ");
 
-            angka += 25;
-            Console.WriteLine(angka);
+            ////casting ke int
+            //int angka = Convert.ToInt32(Console.ReadLine());
+            //if (angka < tanya.Length)
+            //{
+            //    Console.WriteLine($"{angka} lebih kecil daripada {tanya.Length}");
+            //}
+            //else if (angka == tanya.Length)
+            //{
+            //    Console.WriteLine($"{angka} sama dengan {tanya.Length}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{angka} lebih besar daripada {tanya.Length}");
+            //}
 
-            //increment dan degrement
-            angka++;
-            Console.WriteLine(angka);
-            angka--;
-            Console.WriteLine(angka);
+            //angka += 25;
+            //Console.WriteLine(angka);
 
-            // prefiks dan postfiks
-            // ++x dan x++
-            Console.ReadLine();
+            ////increment dan degrement
+            //angka++;
+            //Console.WriteLine(angka);
+            //angka--;
+            //Console.WriteLine(angka);
 
-            //pengulangan 
-            //while statement
-            int x = 1;
-            while (x < 5)
-            {
-                if (x == 4)
-                {
-                    x++;
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine(x);
-                    x++;
-                }
-            }
+            //// prefiks dan postfiks
+            //// ++x dan x++
+            //Console.ReadLine();
 
-            Console.ReadLine();
+            ////pengulangan 
+            ////while statement
+            //int x = 1;
+            //while (x < 5)
+            //{
+            //    if (x == 4)
+            //    {
+            //        x++;
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(x);
+            //        x++;
+            //    }
+            //}
 
-            // do while (checking di awal )
-            int c = 0;
-            do
-            {
-                Console.WriteLine(c);
-                c++;
-            } while (c < 5);
-            Console.ReadLine();
+            //Console.ReadLine();
 
-            // for 
-            // rumus untuk for : for (inisialisasi;kondisi;increment)
-            string batas = "abcde";
-            for (int i = 0; i < batas.Length; i++)
-            {
-                Console.WriteLine(i);
-            }
-            Console.ReadLine();
+            //// do while (checking di awal )
+            //int c = 0;
+            //do
+            //{
+            //    Console.WriteLine(c);
+            //    c++;
+            //} while (c < 5);
+            //Console.ReadLine();
+
+            //// for 
+            //// rumus untuk for : for (inisialisasi;kondisi;increment)
+            //string batas = "abcde";
+            //for (int i = 0; i < batas.Length; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
 
             //foreach i in batas
             //{
             //    Console.WriteLine(i);
             //}
-            
+            string usernameAdmin = "admin";
+            string passwordAdmin = "admin123";
+            do
+            {
+                Console.Write("Masukkan username >> ");
+                string username = Console.ReadLine();
+                Console.Write("masukkan password >> ");
+                string password = Console.ReadLine();
+                if (username == usernameAdmin && password == passwordAdmin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("username dan password yang anda masukkan salah");
+                    Console.Write("tekan enter untuk melanjutkan");
+                    Console.ReadLine();
+                    continue;
+                }
+            } while (true);
+            Console.WriteLine("Login beerhasil selamat datang");
+            Console.ReadLine();
+
 
 
 
